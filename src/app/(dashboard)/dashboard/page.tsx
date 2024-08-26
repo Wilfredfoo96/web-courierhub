d4b863a77d663abf84aed5737906569792361df5
JayNgDigitalCaptain <ngchwanlii@digitalcaptain.io>
2024-08-26 13:30:16 +0800

    init dashboard

---
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+
+import PlaceholderContent from "@/components/placeholder/placeholder-content";
+import { ContentLayout } from "@/components/layout/content-layout";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+
+export default function DashboardPage() {
+  return (
+    <ContentLayout title="Dashboard">
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link href="/">Home</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>Dashboard</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+      <PlaceholderContent />
+    </ContentLayout>
+  );
+}
---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,5 @@
+import DashboardlLayout from "@/components/layout/dashboard-layout";
+
+export default function Dashboard({ children }: { children: React.ReactNode }) {
+  return <DashboardlLayout>{children}</DashboardlLayout>;
+}
